e8fd2d4a6f5382a865e9f5e3d0ebf2da9e362acc
YJ Lee <leeyuejia@gmail.com>
2020-08-24 17:38:38 +0800

    fixed some bugs

--- src/api/index.js
+++ src/api/index.js
@@ -2,12 +2,12 @@ import Axios from 'axios';
 
 const api = Axios.create({
     withCredentials: true,
-    headers: {
-        Accept : "application/json",
-        "Content-Type": "x-www-form-urlencoded"
-    },
-    // baseURL: 'http://localhost:3000/app',
-    baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000/app',
+    // headers: {
+    //     Accept : "application/json",
+    //     "Content-Type": "x-www-form-urlencoded"
+    // },
+    baseURL: 'http://localhost:3000/app',
+    // baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000/app',
 })
 
 // User
